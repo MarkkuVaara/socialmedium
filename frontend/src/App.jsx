d243b27e8b18b95e68c49cc4df7306506318a69d
Markku Vaara <markku.vaara45@gmail.com>
2024-11-20 23:11:19 +0200

    Created first styles for frontend and added filmreel images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,25 @@ import {
   Routes, Route, Link
 } from 'react-router-dom';
 
+import Filmreel from './images/filmreel.png';
+
 const App = () => {
+
+  const [messages, setMessages] = useState([]);
 
   return (
 
-    <div className="main">
+    <div className="mainapp">
       <div className="banner">
         <h1>Movie social</h1>
-        <p>Welcome to the social media for movie watchers!</p>
+        <p>Welcome to the social media for movie, tv and streaming watchers!</p>
+        <img className="filmlogo" src={Filmreel} alt={Filmreel}></img>
+      </div>
+      <div className="nav">
+        <h2>Navigation</h2>
+      </div>
+      <div className="main">
+        <p>Feed</p>
       </div>
     </div>
 
