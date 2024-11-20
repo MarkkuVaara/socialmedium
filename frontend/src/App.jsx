878e8e2d6b924851ad7e88f2de64837f1c9d1f11
Markku Vaara <markku.vaara45@gmail.com>
2024-11-20 20:40:36 +0200

    Added important libraries and created first movie social texts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-);
+import React, { useState, useEffect, useRef } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom';
+
+const App = () => {
+
+  return (
+
+    <div className="main">
+      <div className="banner">
+        <h1>Movie social</h1>
+        <p>Welcome to the social media for movie watchers!</p>
+      </div>
+    </div>
+
+  )
+
+};
 
 export default App;
