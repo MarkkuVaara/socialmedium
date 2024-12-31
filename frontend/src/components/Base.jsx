3f6404d108bb64d19ef0ecd386a8bac38010bde2
Markku Vaara <markku.vaara45@gmail.com>
2024-12-31 23:13:05 +0200

    Changed background black, added button hover effects and moved reply button to the message bottom

--- frontend/src/components/Base.jsx
+++ frontend/src/components/Base.jsx
@@ -55,43 +55,43 @@ const Base = (props) => {
                                     <p className="messagedate">{message.date}</p>
                                     <h4>{message.title}</h4>
                                     <p>{message.message}</p>
-                                    <div>
-                                        <button onClick={() => props.addComment(view.id)}>Reply</button>
-                                    </div>
                                     <div className="reactions">
-                                        {props.likes.map(like => 
-                                        <>
-                                        {like.messageid === message.id &&
-                                            <div className="reaction">
-                                                {like.type === "like" &&
-                                                    <div className="reaction-container">
-                                                        <img className="likeicon" src={likeicon} alt={likeicon} 
-                                                            onClick={() => props.addLike(message.id)}></img>
-                                                        <div className="side-text">
-                                                            {like.amount}
+                                        <button className="replybutton" onClick={() => props.addComment(view.id)}>Reply</button>
+                                        <div>
+                                            {props.likes.map(like => 
+                                            <>
+                                                {like.messageid === message.id &&
+                                                    <div className="reaction">
+                                                        {like.type === "like" &&
+                                                        <div className="reaction-container">
+                                                            <img className="likeicon" src={likeicon} alt={likeicon} 
+                                                                onClick={() => props.addLike(message.id)}></img>
+                                                            <div className="side-text">
+                                                                {like.amount}
+                                                            </div>
                                                         </div>
+                                                        }
+                                                        {like.type === "love" &&
+                                                        <div className="reaction-container">
+                                                            <img src={loveicon} alt={loveicon}></img>
+                                                            <div className="side-text">
+                                                                {like.amount}
+                                                            </div>
+                                                        </div>
+                                                        }
+                                                        {like.type === "unlike" &&
+                                                        <div className="reaction-container">
+                                                            <img src={unlikeicon} alt={unlikeicon}></img>
+                                                            <div className="side-text">
+                                                                {like.amount}
+                                                            </div>
+                                                        </div>
+                                                        }
                                                     </div>
                                                 }
-                                                {like.type === "love" &&
-                                                    <div className="reaction-container">
-                                                        <img src={loveicon} alt={loveicon}></img>
-                                                        <div className="side-text">
-                                                            {like.amount}
-                                                        </div>
-                                                    </div>
-                                                }
-                                                {like.type === "unlike" &&
-                                                    <div className="reaction-container">
-                                                        <img src={unlikeicon} alt={unlikeicon}></img>
-                                                        <div className="side-text">
-                                                            {like.amount}
-                                                        </div>
-                                                    </div>
-                                                }
-                                            </div>
-                                        }
-                                        </>
-                                        )}
+                                            </>
+                                            )}
+                                        </div>
                                     </div>
                                 </div>
                             }
