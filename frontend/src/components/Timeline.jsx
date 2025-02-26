b0e3baa348c13725108537cfdb2047183b5fe0a3
Markku Vaara <markku.vaara45@gmail.com>
2025-02-26 22:00:10 +0200

    Added react bigcalendar to the timeline subpage and modified some calendar styles

--- frontend/src/components/Timeline.jsx
+++ frontend/src/components/Timeline.jsx
@@ -1,9 +1,55 @@
 
+import React, { useState } from 'react';
+
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import { format, parse, startOfWeek, getDay } from 'date-fns';
+import enUS from 'date-fns/locale/en-US';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import './CalendarStyles.css';
+
+const locales = {
+    'en-US': enUS,
+};
+
+const localizer = dateFnsLocalizer({
+    format, parse, startOfWeek,
+    getDay, locales,
+});
+
+import filmimage from '../images/filmreel2.png';
+import message from '../images/messageicon.png';
+import likeicon from '../images/likeicon.png';
+import fadelikeicon from '../images/fadelikeicon.png';
+import loveicon from '../images/loveicon.png';
+import fadeloveicon from '../images/fadeloveicon.png';
+import unlikeicon from '../images/unlikeicon.png';
+import fadeunlikeicon from '../images/fadeunlikeicon.png';
+
+import NewComment from '../components/NewComment';
+import NewView from '../components/NewView';
+
 const Timeline = (props) => {
 
+    const [isMessageOpen, setIsMessageOpen] = useState(null);
+    const [isCommentOpen, setIsCommentOpen] = useState(false);
+    const [isViewOpen, setIsViewOpen] = useState(false);
+
+    const events = [
+        {
+          title: 'Event 1',
+          start: new Date(),
+          end: new Date(),
+        },
+    ];
+
     return (
-        <div className="nav">
-            <h3>Timeline</h3>
+
+        <div className="timelinefeed">
+            
+            <Calendar
+                localizer={localizer} events={events} startAccessor="start" endAccessor="end"
+            />
+
             {props.videos.map(video => 
                 <p>{video.name}</p>
             )}
@@ -17,6 +63,7 @@ const Timeline = (props) => {
                 <p>{like.type}</p>
             )}
         </div>
+
     )
 
 }
