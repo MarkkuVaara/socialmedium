78cc524109cd33c69c162c182affaa9ab9d70d85
Markku Vaara <markku.vaara45@gmail.com>
2025-07-03 19:03:40 +0300

    Created Java Spring backend

---
+++ backend/src/test/java/com/markkuvaara/mediasocial/MediaSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markkuvaara.mediasocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
